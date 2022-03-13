bdbf70bc0a1a0adf7e7b19ac4f91d87b35fb264a
MrGultekin <gultekin.de@gmail.com>
2022-03-13 14:13:02 +0100

    js added with closure func and requestAnimationFrame

---
+++ src/init.js
@@ -0,0 +1,26 @@
+const TICK_RATE = 3000; //It won't change so Capitalized
+
+function tick() {
+    console.log("tick", Date.now());
+}
+
+function init() {
+    console.log('starting game');
+
+    let nextTimeToTick = Date.now(); // let coz it ll be keep changing over the time
+
+    function nextAnimationFrame() { //func in func is closure keep the time tracking // closure to encapsulate the state
+        const now = Date.now();
+
+        if (nextTimeToTick<=now){
+            tick();
+            nextTimeToTick = now + TICK_RATE;
+        }
+
+        requestAnimationFrame(nextAnimationFrame);
+    }
+
+    requestAnimationFrame(nextAnimationFrame); // all browsers ve built in this func does typically JS animation
+}
+
+init();
