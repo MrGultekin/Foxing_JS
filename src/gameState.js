da376e2d295c9e640b87711f0e0f6c11322891f3
MrGultekin <gultekin.de@gmail.com>
2022-03-14 21:16:52 +0100

    Before refactoring

--- src/gameState.js
+++ src/gameState.js
@@ -1,16 +1,25 @@
 import {modFox, modScene} from "./ui";
-import {RAIN_CHANCE, SCENES} from "./constants";
+import {DAY_LENGTH, getNextDieTime, getNextHungerTime, NIGHT_LENGTH, RAIN_CHANCE, SCENES} from "./constants";
 
 const gameState = {
     current: "INIT",
     clock: 1,
     wakeTime: -1, // which currently not WORKING NEGATIVE -1
+    sleepTime: -1,
+    hungryTime: -1,
+    dieTime: -1,
     tick() {
         this.clock++;
         console.log('clock', this.clock);
 
         if (this.clock === this.wakeTime) {
             this.wake()
+        } else if (this.clock === this.sleepTime) {
+            this.sleep();
+        } else if (this.clock === this.hungryTime) {
+            this.getHungry()
+        } else if (this.clock === this.dieTime) {
+            this.die()
         }
 
         return this.clock;
@@ -29,14 +38,31 @@ const gameState = {
         modFox('idling');
         this.scene = Math.random() > RAIN_CHANCE ? 0 : 1;
         modScene(SCENES[this.scene]);
+        this.sleepTime = this.clock + DAY_LENGTH;
+        this.hungryTime = getNextHungerTime(this.clock);
     },
+    sleep() {
+        this.state = 'SLEEP';
+        modFox('sleep');
+        modScene('night');
+        this.wakeTime = this.clock + NIGHT_LENGTH;
+    },
+    getHungry() {
+        this.current = "HUNGRY";
+        this.dieTime = getNextDieTime(this.clock);
+        this.hungryTime = -1;
+        modFox("hungry");
+    },
+    die() {
+        console.log('Dieing')
+    },
+
     handleUserAction(icon) {
         if (
-        ['SLEEP', 'FEEDING', 'CELEBRATING', 'HATCHING'].includes(this.current)) {
+            ['SLEEP', 'FEEDING', 'CELEBRATING', 'HATCHING'].includes(this.current)) {
             // do nothi
             return;
         }
-
 
 
         if (this.current === 'INIT' || this.current === 'DEAD') {
@@ -62,8 +88,10 @@ const gameState = {
     cleanUpPoop() {
         console.log('cleanUpPoop')
     },
-    feed() {
-        console.log('feed')
+    feed() { // can only feed when hungry
+        if (this.current !== 'HUNGRY'){
+            return;
+        }
     }
 
 
